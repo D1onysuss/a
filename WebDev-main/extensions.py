3c78e4a7dc07bf43fc813b06ddafcdbbc5599ef4
D1onysuss <zhaoweiye13@gmail.com>
2025-11-22 18:52:57 +0800

    Delete WebDev-main directory

--- WebDev-main/extensions.py
+++
@@ -1,8 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
-
-db = SQLAlchemy()
-login_manager = LoginManager()
-
-# if user hits a @login_required route, send them here:
-login_manager.login_view = "web.login"
